ee78d6e3531c7e6d276b26ad28eae508d81e2c65
amanias <git@amanyes.com>
2024-05-04 16:36:07 +0200

    Trabajo sobre rama2

--- probadoGit.py
+++ probadoGit.py
@@ -1,3 +1,5 @@
 # Pues tener un fichero para poder probar el funcionamiento de Git ya que no hay nungún archico.
 
 # Cambiando algo para ver que se publica en GitHub
+
+# Pues esta línea solo aparecerá en la Rama2 manteniendo intacta la master.
